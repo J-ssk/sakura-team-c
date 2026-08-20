317b3151c238c59b0b99f0446bb3ee304735cde8
nazo6 <git@nazo6.dev>
2026-08-20 14:22:52 +0900

    Revert "refactor: update server network interface configuration to attach packet filter to shared segment"
    This reverts commit 1837139575e2222757c8fc907e177ffa15076864.

--- infra/terraform/server.tf
+++ infra/terraform/server.tf
@@ -20,14 +20,12 @@ resource "sakura_server" "docker_host" {
   core   = 1
   memory = 1
 
-  # 共有セグメント接続 / プライベートスイッチ接続
+  # 共有セグメント接続
   network_interface = [
+    { upstream = "shared" },
     {
-      upstream         = "shared"
+      upstream         = sakura_vswitch.private_net.id
       packet_filter_id = sakura_packet_filter.intern-team-c.id
-    },
-    {
-      upstream = sakura_vswitch.private_net.id
     },
   ]
 
